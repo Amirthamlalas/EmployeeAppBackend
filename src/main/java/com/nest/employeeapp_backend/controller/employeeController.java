bbe1ea4f245e97755d543e61c77619775a2f3b7e
Amirtha M Lalas <Amirtha@NTP-LAP-746.chn.nestgroup.net>
2022-12-06 15:19:12 +0530

    added connection

--- src/main/java/com/nest/employeeapp_backend/controller/employeeController.java
+++ src/main/java/com/nest/employeeapp_backend/controller/employeeController.java
@@ -1,25 +1,29 @@
 package com.nest.employeeapp_backend.controller;
 
+import com.nest.employeeapp_backend.dao.EmployeeDao;
 import com.nest.employeeapp_backend.model.Employee;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 public class employeeController {
+    @Autowired
+    private EmployeeDao dao;
 
     @GetMapping("/")
         public String HomePage(){
             return "welocome to employee app";
         }
 
-
+    @CrossOrigin(origins = "*")
     @PostMapping(path = "/add",consumes = "application/json",produces = "application/json")
         public  String AddEmployee(@RequestBody Employee e){
         System.out.println(e.getEmpname().toString());
         System.out.println(e.getDesignation().toString());
         System.out.println(e.getCompanyName().toString());
+        dao.save(e);
         return  "Employee added successfully";
     }
 
@@ -31,9 +35,10 @@ public class employeeController {
     public  String EditPage(){
         return "This is Edit page";
     }
+    @CrossOrigin(origins = "*")
     @GetMapping("/view")
-    public  String ViewPage(){
-        return "This is View page";
+    public List<Employee>view(){
+        return (List<Employee>) dao.findAll();
     }
     @PostMapping("/Delete")
     public  String DeletePage(){
